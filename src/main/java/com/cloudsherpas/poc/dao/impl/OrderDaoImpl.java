ac3f693ebf5c72ceb0be4273c617a5f8a2f6fb0a
Alvin Llobrera <alvin.llobrera@cloudsherpas.com>
2015-07-14 21:07:26 +0800

    Added spring context for dependency injection

--- src/main/java/com/cloudsherpas/poc/dao/impl/OrderDaoImpl.java
+++ src/main/java/com/cloudsherpas/poc/dao/impl/OrderDaoImpl.java
@@ -4,9 +4,11 @@ import com.cloudsherpas.poc.dao.DaoManager;
 import com.cloudsherpas.poc.dao.OrderDao;
 import com.cloudsherpas.poc.model.Order;
 import com.googlecode.objectify.Objectify;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public class OrderDaoImpl extends BaseDaoImpl<Order> implements OrderDao {
 
     private static final DaoManager DAO_MANAGER = DaoManager.getInstance();
--- src/main/java/com/cloudsherpas/poc/service/OrderService.java
+++ src/main/java/com/cloudsherpas/poc/service/OrderService.java
@@ -5,16 +5,25 @@ import com.cloudsherpas.poc.dao.impl.OrderDaoImpl;
 import com.cloudsherpas.poc.dto.OrderDTO;
 import com.cloudsherpas.poc.model.Order;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class OrderService {
+
+    @Autowired
+    @Qualifier("orderDao")
+    @Lazy
     private OrderDao orderDao;
+
     private ModelMapper modelMapper;
 
     public OrderService() {
-        orderDao = new OrderDaoImpl();
         modelMapper = new ModelMapper();
     }
 
