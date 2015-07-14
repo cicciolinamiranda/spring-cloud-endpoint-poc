1aa8c5b3b01e8981b1d25a5b73fa73c5690398f4
Jordan Duabe <jordan.duabe@cloudsherpas.com>
2015-07-14 18:39:12 +0800

    Update Customer and Product DTO properties
    Removed errant id property on the Customer DTO along with corresponding
    accessor methods
    Added the id property and corresponding accessor methods to the Product
    DTO for use on Datastore queries

--- src/main/java/com/cloudsherpas/poc/dto/ProductDTO.java
+++ src/main/java/com/cloudsherpas/poc/dto/ProductDTO.java
@@ -1,8 +1,17 @@
 package com.cloudsherpas.poc.dto;
 
 public class ProductDTO {
+    private String id;
     private String name;
     private int stock;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
